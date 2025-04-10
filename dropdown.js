57d5bf7d4174089d5166a27410ecaf5f62a6b8c7
Giulio Riondino <riondino.giulio@gmail.com>
2025-04-09 21:06:32 -0700

    Add global dropdown close trigger

--- dropdown.js
+++ dropdown.js
@@ -1,5 +1,17 @@
 const dropdownButtons = document.querySelectorAll('.toggle');
 
+document.addEventListener('click', (event) => {
+  const menu = event.target.closest('.menu');
+  const toggle = event.target.closest('.toggle');
+
+  console.log(menu, toggle);
+
+  if (!menu && !toggle) {
+    const dropdownMenus = document.querySelectorAll('.dropdown .menu');
+    dropdownMenus.forEach((menu) => menu.classList.remove('visible'));
+  }
+});
+
 dropdownButtons.forEach((dropdownButton) =>
   dropdownButton.addEventListener('click', () => {
     const dropdown = dropdownButton.parentElement;
