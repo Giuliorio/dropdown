084328911637b14607167828a6b9885c9d3a7634
Giulio Riondino <riondino.giulio@gmail.com>
2025-04-09 21:12:25 -0700

    Close previously open dropdowns when opening a new one

--- dropdown.js
+++ dropdown.js
@@ -4,11 +4,8 @@ document.addEventListener('click', (event) => {
   const menu = event.target.closest('.menu');
   const toggle = event.target.closest('.toggle');
 
-  console.log(menu, toggle);
-
   if (!menu && !toggle) {
-    const dropdownMenus = document.querySelectorAll('.dropdown .menu');
-    dropdownMenus.forEach((menu) => menu.classList.remove('visible'));
+    closeAllDropdowns();
   }
 });
 
@@ -17,6 +14,12 @@ dropdownButtons.forEach((dropdownButton) =>
     const dropdown = dropdownButton.parentElement;
     const menu = dropdown.querySelector('.menu');
 
+    closeAllDropdowns();
     menu.classList.toggle('visible');
   })
 );
+
+function closeAllDropdowns() {
+  const dropdownMenus = document.querySelectorAll('.dropdown .menu');
+  dropdownMenus.forEach((menu) => menu.classList.remove('visible'));
+}
