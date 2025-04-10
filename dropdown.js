fc2e1acff3868fb7a8e082cbbfff7f95b0f881b8
Giulio Riondino <riondino.giulio@gmail.com>
2025-04-09 22:15:30 -0700

    Select menu items in open menu with arrow keys

--- dropdown.js
+++ dropdown.js
@@ -13,6 +13,14 @@ document.addEventListener('keydown', (event) => {
   if (event.key === 'Escape') {
     closeAllDropdowns();
   }
+
+  if (event.key === 'ArrowDown') {
+    handleArrowNavigation('down');
+  }
+
+  if (event.key === 'ArrowUp') {
+    handleArrowNavigation('up');
+  }
 });
 
 dropdownButtons.forEach((dropdownButton) =>
@@ -22,10 +30,31 @@ dropdownButtons.forEach((dropdownButton) =>
 
     closeAllDropdowns();
     menu.classList.toggle('visible');
+
+    const firstItem = menu.querySelector('.item');
+    console.log(firstItem);
+    if (firstItem) firstItem.focus();
   })
 );
 
 function closeAllDropdowns() {
   const dropdownMenus = document.querySelectorAll('.dropdown .menu');
   dropdownMenus.forEach((menu) => menu.classList.remove('visible'));
+}
+
+function handleArrowNavigation(direction) {
+  const menu = document.querySelector('.dropdown .visible');
+  if (!menu) return;
+
+  const items = [...menu.querySelectorAll('.item')];
+
+  let currentIndex = items.findIndex((item) => item === document.activeElement);
+
+  if (direction == 'down') {
+    currentIndex = currentIndex === items.length - 1 ? 0 : (currentIndex = 1);
+  } else if (direction === 'up') {
+    currentIndex = currentIndex === 0 ? items.length - 1 : currentIndex - 1;
+  }
+
+  items[currentIndex].focus();
 }
