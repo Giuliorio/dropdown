d9e62fc7b6f0a9508bcbfabb04a561e2d1db24d2
Giulio Riondino <riondino.giulio@gmail.com>
2025-04-12 12:19:51 -0700

    Install webpack

---
+++ webpack.common.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/dropdown.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
